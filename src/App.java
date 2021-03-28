de90189b280596dc7b3aac737e97c006905b48e9
IvyChee <hiwhoami@hotmail.com>
2021-03-28 15:19:53 -0400

    Update App.java

--- src/App.java
+++ src/App.java
@@ -1,7 +1,7 @@
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Car myFerrari = new Car("hello","488"); 
+        Car myFerrari = new Car("hhhh","488"); 
         myFerrari.moveForward();
         myFerrari.brake();
     }
